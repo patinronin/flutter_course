e7b2159c79fdff0891069322c7a2f3216832635a
patinronin <jglg191995@gmail.com>
2022-03-27 23:29:10 -0600

    add more examples

---
+++ dart_course/mixins.dart
@@ -0,0 +1,44 @@
+abstract class Animal {}
+
+abstract class Mamifero extends Animal {}
+
+abstract class Ave extends Animal {}
+
+abstract class Pez extends Animal {}
+
+abstract class Volador {
+  void volar() => print("Estoy volando");
+}
+
+abstract class Caminante {
+  void caminar() => print("Estoy caminando");
+}
+
+abstract class Nadador {
+  void nadar() => print("Estoy nadando");
+}
+
+class Delfin extends Mamifero with Nadador {}
+
+class Murcielago extends Mamifero with Volador, Caminante {}
+
+class Gato extends Mamifero with Caminante {}
+
+class Paloma extends Ave with Caminante, Volador {}
+
+class Pato extends Ave with Volador, Caminante, Nadador {}
+
+class Tiburon extends Pez with Nadador {}
+
+class PezVolador extends Pez with Nadador, Volador {}
+
+void main() {
+  final flipper = new Delfin();
+  flipper.nadar();
+
+  final pato = new Pato();
+
+  pato.caminar();
+  pato.volar();
+  pato.nadar();
+}
